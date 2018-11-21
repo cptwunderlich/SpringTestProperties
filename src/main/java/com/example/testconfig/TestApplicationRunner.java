9207f72c68c1ea77588707cf934c348ae43a134a
Benjamin Maurer <maurer.benjamin@gmail.com>
2018-11-21 10:57:25 +0100

    Added example project

---
+++ src/main/java/com/example/testconfig/TestApplicationRunner.java
@@ -0,0 +1,22 @@
+package com.example.testconfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Benjamin Maurer (benjamin.maurer@dataphone.at)
+ * @since 21.11.2018
+ */
+@Service
+public class TestApplicationRunner implements ApplicationRunner {
+
+	@Value("${my.test.property}")
+	private String testproperty;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("***Value is: " + testproperty);
+	}
+}
---
+++ src/test/java/com/example/testconfig/TestconfigApplicationTests.java
@@ -0,0 +1,28 @@
+package com.example.testconfig;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class TestconfigApplicationTests {
+
+	@Value("${my.test.property}")
+	private String testproperty;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void assertValue() {
+		Assert.assertEquals("foobar", testproperty);
+	}
+
+}
