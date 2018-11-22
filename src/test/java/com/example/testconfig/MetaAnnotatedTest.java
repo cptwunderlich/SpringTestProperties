a91d3acc46ed757e9ed309755f21f5cc3e0b269c
Benjamin Maurer <maurer.benjamin@gmail.com>
2018-11-22 13:38:58 +0100

    Fixed the tests, it does actually work with meta-annotations....

--- src/test/java/com/example/testconfig/MetaAnnotatedTest.java
+++ src/test/java/com/example/testconfig/MetaAnnotatedTest.java
@@ -2,8 +2,13 @@ package com.example.testconfig;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 @TestAnnotation
 public class MetaAnnotatedTest {
 	@Value("${my.test.property}")
--- src/test/java/com/example/testconfig/TestAnnotation.java
+++ src/test/java/com/example/testconfig/TestAnnotation.java
@@ -1,7 +1,15 @@
 package com.example.testconfig;
 
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import org.springframework.test.context.ActiveProfiles;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
 @ActiveProfiles("test")
+@Retention(RUNTIME)
+@Target(TYPE)
 public @interface TestAnnotation {
 }
